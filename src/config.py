5ab5c641e1aa7e089295b462d968c7ccc9993a22
gfedorov7 <fedorov.programming@gmail.com>
2025-07-22 00:37:32 +0300

    edit config

--- src/config.py
+++ src/config.py
@@ -13,8 +13,11 @@ class OAuthSettings(BaseSettings):
     url: str = "http://localhost:8000/"
     endpoint_callback: str = "auth/login/callback"
     state_token_key: str = "oauth_state"
+    current_token_type: str = "Bearer"
+
     google_auth_url: str = "https://accounts.google.com/o/oauth2/v2/auth?"
     google_exchange_url: str = "https://oauth2.googleapis.com/token"
+    google_valid_access_token_url: str = "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token="
 
     client_id: str
     client_secret: str
