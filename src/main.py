441fefe6c87b087993b9c4797b849e4bf73ba19e
gfedorov7 <fedorov.programming@gmail.com>
2025-07-22 00:58:50 +0300

    add middlewares

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.logs import setup_logging
 from src.exceptions import AppException
@@ -9,6 +10,20 @@ from src.api.routers import auth_router
 setup_logging()
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth_router)
 
 @app.exception_handler(AppException)
